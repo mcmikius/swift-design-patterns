d171bed9f1282deba724d95628b7acbed3b725ce
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 10:18:54 +0200

    init strategy

---
+++ Strategy.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+class Human {
+    func swim() {
+        print("swimming")
+    }
+    func dive() {
+        print("diving")
+    }
+    func run() {
+        print("running")
+    }
+}
+
+let human = Human()
+
+human.dive()
