e8cf321787c284ff65a9f4a53bee8108a4b4ffaf
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 11:13:04 +0200

    init decorator

---
+++ Decorator.playground/Contents.swift
@@ -0,0 +1,66 @@
+
+protocol Porsche {
+    func getPrice() -> Double
+    func getDescription() -> String
+}
+
+class Boxter: Porsche {
+    func getPrice() -> Double {
+        return 120
+    }
+    
+    func getDescription() -> String {
+        return "Porsche Boxter"
+    }
+}
+
+class Panamerra: Porsche {
+    func getPrice() -> Double {
+        return 150
+    }
+    
+    func getDescription() -> String {
+        return "Porsche Panamerra"
+    }
+}
+
+class PorcheDecorator: Porsche {
+
+    private let decoratedPorsche: Porsche
+    
+    required init(decoratedPorsche: Porsche) {
+        self.decoratedPorsche = decoratedPorsche
+    }
+    func getPrice() -> Double {
+        return decoratedPorsche.getPrice()
+    }
+    
+    func getDescription() -> String {
+        return decoratedPorsche.getDescription()
+    }
+}
+
+class PremiumAudioSystem: PorcheDecorator {
+    required init(decoratedPorsche: Porsche) {
+        super.init(decoratedPorsche: decoratedPorsche)
+    }
+    override func getPrice() -> Double {
+        return super.getPrice() + 30
+    }
+    
+    override func getDescription() -> String {
+        return super.getDescription() + " with premium audio system"
+    }
+}
+
+class PanoramicSunroof: PorcheDecorator {
+    required init(decoratedPorsche: Porsche) {
+        super.init(decoratedPorsche: decoratedPorsche)
+    }
+    override func getPrice() -> Double {
+        return super.getPrice() + 20
+    }
+    override func getDescription() -> String {
+        return super.getDescription() + " with panoramic sunroof"
+    }
+}
