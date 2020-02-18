c03fe0974c842e56934211204cb8e7ac11b508ac
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 11:48:25 +0200

    abstract factory init

--- AbstractFactory.playground/Contents.swift
+++ AbstractFactory.playground/Contents.swift
@@ -1,1 +1,30 @@
+protocol Car {
+    func drive()
+}
+class LittleCar: Car {
+    func drive() {
+        print("drive a little size car")
+    }
+}
+
+class MiddleCar: Car {
+    func drive() {
+        print("drive a middle size car")
+    }
+}
+
+protocol Bus {
+    func drive()
+}
+class LittleBus: Bus {
+    func drive() {
+        print("drive a little size bus")
+    }
+}
+
+class MiddleBus: Bus {
+    func drive() {
+        print("drive a middle size bus")
+    }
+}
 
