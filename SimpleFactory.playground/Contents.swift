d20ab47ee32b1fa2acc93e47434072f99a7cf57c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 11:29:09 +0200

    add factory

--- SimpleFactory.playground/Contents.swift
+++ SimpleFactory.playground/Contents.swift
@@ -1,3 +1,39 @@
-import UIKit
 
-var str = "Hello, playground"
+enum CarType {
+    case huge
+    case fast
+}
+
+protocol Car {
+     func drive()
+}
+
+class HugeCar: Car {
+    func drive() {
+        print("you drive huge car")
+    }
+}
+
+class FastCar: Car {
+    func drive() {
+        print("you drive fast car")
+    }
+}
+
+let hugeCar = HugeCar()
+hugeCar.drive()
+let fastCar = FastCar()
+fastCar.drive()
+
+class CarFactory {
+    static func produceCar(type: CarType) -> Car {
+        var car: Car
+        switch type {
+        case .fast:
+            car = FastCar()
+        case .huge:
+            car = HugeCar()
+        }
+        return car
+    }
+}
