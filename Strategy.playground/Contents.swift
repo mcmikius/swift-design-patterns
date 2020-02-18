7e73a5a8c9d82dc1872e1fc580ba2790c4873192
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 10:32:38 +0200

    lesson done

--- Strategy.playground/Contents.swift
+++ Strategy.playground/Contents.swift
@@ -1,16 +1,70 @@
 
-class Human {
+protocol SwimBehaivor {
+    func swim()
+}
+
+class ProfessionalSwimmer: SwimBehaivor {
     func swim() {
-        print("swimming")
+        print("profssional swimming")
     }
-    func dive() {
-        print("diving")
-    }
-    func run() {
-        print("running")
+}
+class NonSwimmer: SwimBehaivor {
+    func swim() {
+        print("non swimming")
     }
 }
 
-let human = Human()
 
-human.dive()
+protocol DiveBehavior {
+    func dive()
+}
+
+class ProfessionalDiver: DiveBehavior {
+    func dive() {
+        print("professional diving")
+    }
+}
+class NewbieDiver: DiveBehavior {
+    func dive() {
+        print("newbie diving")
+    }
+}
+class NonDiver: DiveBehavior {
+    func dive() {
+        print("non diving")
+    }
+}
+
+
+class Human {
+    
+    private var diveBehavior: DiveBehavior!
+    private var swimBehavior: SwimBehaivor!
+    
+    func performSwim() {
+        swimBehavior.swim()
+    }
+    func performDive() {
+        diveBehavior.dive()
+    }
+    
+    func setSwimBehavior(swimBehavior: SwimBehaivor) {
+        self.swimBehavior = swimBehavior
+    }
+    
+    func setDiveBehavior(diveBehavior: DiveBehavior) {
+        self.diveBehavior = diveBehavior
+    }
+    
+    func run() {
+        print("running")
+    }
+    init(swimBehavior: SwimBehaivor, diveBehavior: DiveBehavior) {
+        self.swimBehavior = swimBehavior
+        self.diveBehavior = diveBehavior
+    }
+}
+
+let human = Human(swimBehavior: ProfessionalSwimmer(), diveBehavior: ProfessionalDiver())
+human.performDive()
+human.performSwim()
