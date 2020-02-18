e695d9a17f35c3e789f2d08b9bec6c2817637042
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-18 12:38:42 +0200

    adapter done

--- Adapter.playground/Contents.swift
+++ Adapter.playground/Contents.swift
@@ -1,1 +1,33 @@
 
+//adaptee
+class SimpleCar {
+    func sound() -> String {
+        return "tr-tr-tr"
+    }
+}
+// target
+protocol SuperCarProtocol {
+    func makeNoise() -> String
+}
+
+
+class SuperCar: SuperCarProtocol {
+    func makeNoise() -> String {
+        return "whroom - whroom"
+    }
+    
+    
+}
+
+//adaptor
+class SuperCarAdapter: SuperCarProtocol {
+    
+    var simpleCar: SimpleCar
+    init(simpleCar: SimpleCar) {
+        self.simpleCar = simpleCar
+    }
+    
+    func makeNoise() -> String {
+        return simpleCar.sound()
+    }
+}
