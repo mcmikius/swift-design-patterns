7f7f256ba3f282216029fc12b4f4f6bf663eb80c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 12:13:31 +0200

    builder done

--- Builder.playground/Contents.swift
+++ Builder.playground/Contents.swift
@@ -1,1 +1,48 @@
+import UIKit
 
+protocol ThemeProtocol {
+    var backgroundColor: UIColor { get }
+    var textColor: UIColor { get }
+}
+
+class Theme: ThemeProtocol {
+    var backgroundColor: UIColor
+    var textColor: UIColor
+    
+    init(backgroundColor: UIColor, textColor: UIColor) {
+        self.backgroundColor = backgroundColor
+        self.textColor = textColor
+    }
+}
+
+protocol ThemeBuilderProtocol {
+    func setBackgroundColor(color: UIColor)
+    func setText(color: UIColor)
+    func createTheme() -> ThemeProtocol?
+}
+
+class ThemeBuilder: ThemeBuilderProtocol {
+    private var backgroundColor: UIColor?
+    private var textColor: UIColor?
+    func setBackgroundColor(color: UIColor) {
+        self.backgroundColor = color
+    }
+    
+    func setText(color: UIColor) {
+        self.textColor = color
+    }
+    
+    func createTheme() -> ThemeProtocol? {
+        guard let backgroundColor = backgroundColor, let textColor = textColor else {
+            return nil
+        }
+        return Theme(backgroundColor: backgroundColor, textColor: textColor)
+    }
+    
+    
+}
+let builder = ThemeBuilder()
+builder.setText(color: .white)
+builder.setBackgroundColor(color: .black)
+let theme = builder.createTheme()
+print(theme ?? "")
