147da0fbeb7c299de93a4063175e0ebfc9da3ee7
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 09:08:48 +0200

    template init

--- TemplateMethod.playground/Contents.swift
+++ TemplateMethod.playground/Contents.swift
@@ -1,1 +1,27 @@
 
+class DriveVehicle {
+    
+    func startVehicle() {
+        
+    }
+    
+    func haveASeat() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func useProtection() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func lookAtTheMirror() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func turnSignal() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func driveForward() {
+        preconditionFailure("this method should be overriden")
+    }
+}
