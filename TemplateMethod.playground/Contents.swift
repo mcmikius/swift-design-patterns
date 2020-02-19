ebac2e7a62ff554dca5145a89c0fc03c89a936b6
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 09:16:08 +0200

    with class

--- TemplateMethod.playground/Contents.swift
+++ TemplateMethod.playground/Contents.swift
@@ -1,8 +1,12 @@
 
 class DriveVehicle {
     
-    func startVehicle() {
-        
+    final func startVehicle() {
+        haveASeat()
+        useProtection()
+        lookAtTheMirror()
+        turnSignal()
+        driveForward()
     }
     
     func haveASeat() {
@@ -25,3 +29,54 @@ class DriveVehicle {
         preconditionFailure("this method should be overriden")
     }
 }
+
+class Bycicle: DriveVehicle {
+    override func haveASeat() {
+        print("sit down on a bicycle seat")
+    }
+    
+    override func useProtection() {
+        print("wear a helmet")
+    }
+    
+    override func lookAtTheMirror() {
+        print("look at the little mirror")
+    }
+    
+    override func turnSignal() {
+        print("show left hand")
+    }
+    
+    override func driveForward() {
+        print("padal")
+    }
+    
+}
+
+class Car: DriveVehicle {
+    override func haveASeat() {
+        print("sit down on a car seat")
+    }
+    
+    override func useProtection() {
+        print("fasten seat belt")
+    }
+    
+    override func lookAtTheMirror() {
+        print("look at the review mirror")
+    }
+    
+    override func turnSignal() {
+        print("turn on left turn light")
+    }
+    
+    override func driveForward() {
+        print("push padal")
+    }
+    
+}
+
+let car = Car()
+let bicycle = Bycicle()
+car.startVehicle()
+bicycle.startVehicle()
