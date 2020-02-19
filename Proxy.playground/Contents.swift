ab2dcf454b11e5e3ecf8c560a49e31aeb95a1afe
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 11:52:11 +0200

    first variant

--- Proxy.playground/Contents.swift
+++ Proxy.playground/Contents.swift
@@ -1,1 +1,36 @@
+class User {
+    let id = "123"
+}
 
+protocol ServerProtocol {
+    func grandAccess(user: User)
+    func denyAccess(user: User)
+}
+
+class ServerSide: ServerProtocol {
+    func grandAccess(user: User) {
+        print("access granted to user with id = \(user.id)")
+    }
+    
+    func denyAccess(user: User) {
+        print("access denied to user with id = \(user.id)")
+    }
+}
+
+class ServerProxy: ServerProtocol {
+    
+    lazy private var server: ServerSide = ServerSide()
+    
+    func grandAccess(user: User) {
+        server.grandAccess(user: user)
+    }
+    
+    func denyAccess(user: User) {
+        server.denyAccess(user: user)
+    }
+}
+
+let user = User()
+let proxy = ServerProxy()
+proxy.grandAccess(user: user)
+proxy.denyAccess(user: user)
