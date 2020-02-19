047952a85b7cc290309ced0d2c40c06c45b94cf2
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-02-19 12:27:18 +0200

    done

--- ChainOfResponsibility.playground/Contents.swift
+++ ChainOfResponsibility.playground/Contents.swift
@@ -1,3 +1,61 @@
-import UIKit
+class Enemy {
+    let strength = 300
+}
 
-var str = "Hello, playground"
+protocol MilitaryChain {
+    var strength: Int { get }
+    var nextRank: MilitaryChain? { get set }
+    func shouldDefeatWithStrength(amount: Int)
+}
+
+class Soldier: MilitaryChain {
+    var strength: Int = 100
+    
+    var nextRank: MilitaryChain?
+    
+    func shouldDefeatWithStrength(amount: Int) {
+        if amount < strength {
+            print("Soldier got it")
+        } else {
+            nextRank?.shouldDefeatWithStrength(amount: amount)
+        }
+    }
+}
+
+class Officer: MilitaryChain {
+    var strength: Int = 500
+    
+    var nextRank: MilitaryChain?
+    
+    func shouldDefeatWithStrength(amount: Int) {
+        if amount < strength {
+            print("Officer got it")
+        } else {
+            nextRank?.shouldDefeatWithStrength(amount: amount)
+        }
+    }
+}
+
+class General: MilitaryChain {
+    var strength: Int = 1000
+    
+    var nextRank: MilitaryChain?
+    
+    func shouldDefeatWithStrength(amount: Int) {
+        if amount < strength {
+            print("General got it")
+        } else {
+            print("We can't defeat this enemy")
+        }
+    }
+}
+
+let enemy = Enemy()
+let soldier = Soldier()
+let officer = Officer()
+let general = General()
+
+soldier.nextRank = officer
+officer.nextRank = general
+
+soldier.shouldDefeatWithStrength(amount: enemy.strength)
